512c60ea7ed37e47ed8632544d653023ca7fc780
prolewall <h.kowalik98@gmail.com>
2020-11-08 20:07:26 +0100

    Add abstract class

---
+++ sql_abstract_class.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class SqlDataClass(ABC):
+    @abstractmethod
+    def generate_instance(self):
+        pass
+
+    @abstractmethod
+    def generate_sql(self):
+        pass
