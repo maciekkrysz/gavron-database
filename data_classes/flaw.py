47af06be4760fe3ddc48b282aaab21f34e553805
prolewall <h.kowalik98@gmail.com>
2020-11-08 20:35:50 +0100

    data class structure

---
+++ data_classes/flaw.py
@@ -0,0 +1,9 @@
+from data_classes.sql_abstract_class import SqlDataClass
+
+
+class Flaw(SqlDataClass):
+    def generate_sql(self):
+        pass
+
+    def generate_instance(self):
+        pass
---
+++ data_classes/flight_schedule.py
@@ -0,0 +1,10 @@
+from data_classes.sql_abstract_class import SqlDataClass
+
+
+class FlightSchedule(SqlDataClass):
+    def generate_sql(self):
+        pass
+
+    def generate_instance(self):
+        pass
+
