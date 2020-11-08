4c0c7a88fd0137c63992512e10e40754a437ec1c
maciekkrysz <63737298+maciekkrysz@users.noreply.github.com>
2020-11-08 20:10:42 +0100

    UPDATE: main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 
 
-generate():
-    
+def generate():
+    # doPointType()
+    # doObjType()
+    # doDrone()
+    # doRole()
 
+    # doRoute()
+    # doPoint()
+    # doPointOnRoute()
 
+    # doAccount()
+    # doUser()
 
+    # doFlightSchedule()
+    # doFlight()
+    # doLog()
+    # doFlaw()
 
-
+    # doObject()
+    pass
 
 
 generate()
