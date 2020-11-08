a3eac3fc1e79f3cb351d2dac8b3f443a96beae97
kadisin <38622355+kadisin@users.noreply.github.com>
2020-11-08 23:23:06 +0100

    Update point.py

--- data_classes/point.py
+++ data_classes/point.py
@@ -1,13 +1,12 @@
-from data_classes.sql_abstract_class import SqlDataClass
+import values
+import random
 
-
-class Point(SqlDataClass):
-    def generate_sql(self):
-        pass
-
-    def generate_instance(self):
-        pass
+class Point():
 
     @staticmethod
     def generate_all():
-        pass
+        list = []
+        for i in range(values.POINT_LEN):
+            list.append([random.randrange(1,values.POINTTYPE_LEN-1),random.randrange(values.Wroclaw_Longitude-5,values.Wroclaw_Longitude+5),random.randrange(values.Wroclaw_Latitude-5,values.Wroclaw_Latitude+5)])
+        print(list)
+
