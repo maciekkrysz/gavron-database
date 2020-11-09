a383f389becc0898f1b7ce368ccb2b0575e30570
prolewall <h.kowalik98@gmail.com>
2020-11-09 20:37:48 +0100

    add to co maciej chcial

--- data_classes/point.py
+++ data_classes/point.py
@@ -1,11 +1,14 @@
 import values
 import random
 
+from utilities import sql_file_utils
+
 class Point():
 
     @staticmethod
     def generate_all():
         list = []
         for i in range(values.POINT_LEN):
-            list.append([random.randrange(1,values.POINTTYPE_LEN-1),round(random.uniform(values.Wroclaw_Longitude-5,values.Wroclaw_Longitude+5),5),round(random.uniform(values.Wroclaw_Latitude-5,values.Wroclaw_Latitude+5),5)])
+            list.append([random.randrange(1,values.POINTTYPE_LEN-1),round(random.uniform(values.Wroclaw_Longitude-1,values.Wroclaw_Longitude+1),5),round(random.uniform(values.Wroclaw_Latitude-1,values.Wroclaw_Latitude+1),5)])
        
+        sql_file_utils.addToFile("point", ["IdPointType", "Longitude", "Latitude"], list)
