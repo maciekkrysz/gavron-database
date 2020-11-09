079a8a02c97dcc408e3a516a6165f60defae6de3
kadisin <38622355+kadisin@users.noreply.github.com>
2020-11-09 19:16:51 +0100

    Update object.py

--- data_classes/object.py
+++ data_classes/object.py
@@ -1,13 +1,13 @@
-from data_classes.sql_abstract_class import SqlDataClass
+from utilities import sql_file_utils
+import values
+import random
 
-
-class Object(SqlDataClass):
-    def generate_sql(self):
-        pass
-
-    def generate_instance(self):
-        pass
-
+class Object():
     @staticmethod
     def generate_all():
-        pass
+        list = []
+        for i in range(values.OBJECT_LEN):
+            link = "/" + str(random.randrange(1,10000)) + "." + str(random.randrange(1000,10000))
+            list.append([random.randrange(1,values.FLIGHT_LEN),random.randrange(1,values.OBJECTTYPE_LEN),link])
+
+        sql_file_utilis.addToFile("object",["IdFlight","IdObjectType","PathToPhoto"],list)
