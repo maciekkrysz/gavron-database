d58e125d2f7b903b812c9bfc54f3dcd0218ae4d8
Maciek Krysz <maciekkrysz1@gmail.com>
2020-11-11 23:22:48 +0100

    flight_schedule and point works

--- data_classes/flight_schedule.py
+++ data_classes/flight_schedule.py
@@ -1,6 +1,7 @@
 from data_classes.sql_abstract_class import SqlDataClass
 from utilities.generator_utils import rand_string, rand_date_in_range
 from utilities.sql_file_utils import addToFile
+from utilities.db_conn_utilities import get_all_index
 from values import FLIGHTSCHEDULE_LEN, USER_LEN, ROUTE_LEN
 
 import random
@@ -18,24 +19,26 @@ class FlightSchedule(SqlDataClass):
     def generate_sql(self) -> str:
         return [self.__id_user, self.__id_route, self.__start_date, self.__start_minute, self.__interval]
 
-    def generate_instance(self):
-        self.__id_user = random.randint(1, USER_LEN)
+    def generate_instance(self, user_indexes, route_indexes):
+        random_user = random.randint(0, len(user_indexes) - 1)
+        random_route = random.randint(0, len(route_indexes) - 1)
 
-        self.__id_route = random.randint(1, ROUTE_LEN)
-
+        self.__id_user = user_indexes[random_user]
+        self.__id_route = route_indexes[random_route]
         self.__start_date = rand_date_in_range("2019-11-01", "2019-12-31")
-
         self.__start_minute = random.randint(0, 1440)
-
         self.__interval = random.randrange(60, 43200, 60)
 
     @staticmethod
     def generate_all(cursor):
         values = []
 
+        user_indexes = get_all_index(cursor, 'user')
+        route_indexes = get_all_index(cursor, 'route')
+
         instance = FlightSchedule()
         for _ in range(FLIGHTSCHEDULE_LEN):
-            instance.generate_instance()
+            instance.generate_instance(user_indexes, route_indexes)
             sql_string = instance.generate_sql()
             values.append(sql_string)
 
--- data_classes/point.py
+++ data_classes/point.py
@@ -1,3 +1,4 @@
+from utilities.db_conn_utilities import get_all_index
 import values
 import random
 
@@ -10,8 +11,18 @@ class Point():
     def generate_all(cursor):
         lista = []
         for i in range(values.POINT_LEN):
-            lista.append([random.randrange(1, values.POINTTYPE_LEN-1), round(random.uniform(values.Wroclaw_Longitude-1,
-                                                                                            values.Wroclaw_Longitude+1), 5), round(random.uniform(values.Wroclaw_Latitude-1, values.Wroclaw_Latitude+1), 5)])
+            point_type_indexes = get_all_index(cursor, 'pointtype')
+            random_point_type = random.randint(0, len(point_type_indexes) - 1)
+
+            idPoint = point_type_indexes[random_point_type]
+
+            longitude = round(random.uniform(
+                values.Wroclaw_Longitude-1, values.Wroclaw_Longitude+1), 5)
+
+            latitude = round(random.uniform(
+                values.Wroclaw_Latitude-1, values.Wroclaw_Latitude+1), 5)
+
+            lista.append([idPoint, longitude, latitude])
 
         sql_file_utils.addToFile(cursor,
                                  "point", ["IdPointType", "Longitude", "Latitude"], lista)
